a6cce1b4742335590d6a8066c1422573eb3995d1
Agustin Masuelli <agus.masu@gmail.com>
2017-11-20 12:34:06 -0300

    Deleted timeout

--- client/routes.js
+++ client/routes.js
@@ -15,51 +15,45 @@ router.get('/', function(req, res, next) {
   res.sendFile(__dirname + "/index.html");
 });
 
-/* setTimeOut runs the operaion after n time
- * n (used a the end) is in seconds, is multiplied by 1000 because we need miniseconds
- * each time n seconds passes the server will post a new image
- * The next method is asynchronous, so it doesnt stop all the server*/
-var n = 4;
-setTimeout(function(){
-    router.post('/photo',upload.single('imageUpload'), function(req, res){
-        var uploadedFile = req.file.buffer;
-        console.log(uploadedFile);
+router.post('/photo',upload.single('imageUpload'), function(req, res){
+    var uploadedFile = req.file.buffer;
+    console.log(uploadedFile);
 
-        var server = net.createServer(function(socket) {
-            socket.on('data', function(data){
-                console.log(data);
-                var coor = new Buffer(data, 'base64').toString("ascii");
-                console.log(coor);
-                coordinates = coor.split(" ");
-                const x = Math.floor(coordinates[0]*15 + ORIGIN_X);
-                const y = Math.floor(coordinates[2]*15 + ORIGIN_Y);
-                if(coordinates[1] != -1){
-                    res.render('map', {x: x, y: y});
-                }
-                else {
-                    res.render('error');
-                }
-                server.close();
-            });
-        });
-
-        server.listen(7001, '127.0.0.1');
-
-        var ip = '';
-        var port = 7000;
-
-        var buffer = new Buffer(uploadedFile).toString('base64');
-        client.connect(port, ip, function() {
-            console.log('Connected');
-            console.log('Writing...');
-            console.log(buffer.byteLength);
-            client.write(buffer, function (err){
-                console.log(err);
-                console.log('Wrote !!');
-                client.end();
-            });
+    var server = net.createServer(function(socket) {
+        socket.on('data', function(data){
+            console.log(data);
+            var coor = new Buffer(data, 'base64').toString("ascii");
+            console.log(coor);
+            coordinates = coor.split(" ");
+            const x = Math.floor(coordinates[0]*15 + ORIGIN_X);
+            const y = Math.floor(coordinates[2]*15 + ORIGIN_Y);
+            if(coordinates[1] != -1){
+                res.render('map', {x: x, y: y});
+            }
+            else {
+                res.render('error');
+            }
+            server.close();
         });
     });
-}, n * 1000);
+
+    server.listen(7001, '127.0.0.1');
+
+    var ip = '';
+    var port = 7000;
+
+    var buffer = new Buffer(uploadedFile).toString('base64');
+    client.connect(port, ip, function() {
+        console.log('Connected');
+        console.log('Writing...');
+        console.log(buffer.byteLength);
+        client.write(buffer, function (err){
+            console.log(err);
+            console.log('Wrote !!');
+            client.end();
+        });
+    });
+});
+
 
 module.exports = router;
